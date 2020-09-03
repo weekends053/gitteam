3d6a6136922761fc0108f911a71994420c42d5f9
riw <614259040@webmail.npru.ac.th>
2020-09-03 15:21:21 +0700

    Implement Multiply

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ public class Calculator{
 		return num1+num2;
 	}
 	public int multiply(int num1,int num2){
-		return 0;
+		return num1*num2;
 	}
 	public int subtract(int num1,int num2){
 		return 0;
