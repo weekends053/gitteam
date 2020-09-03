70873ccf336694df226bd3cacdccc73677e628a1
Naruapon Suwanwijit <naruapon@npru.ac.th>
2020-09-03 14:45:45 +0700

    Create Sturcture

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator{
+	public int add(int num1,int num2){
+		return 0;
+	}
+	public int multiply(int num1,int num2){
+		return 0;
+	}
+	public int subtract(int num1,int num2){
+		return 0;
+	}
+	public double divide(int num1, int num2){
+		return 0;
+	}
+}
