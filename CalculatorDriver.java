128632b91c8cbb7b0253f0af27d65d9683bc6c2b
Naruapon Suwanwijit <naruapon@npru.ac.th>
2020-09-03 16:02:14 +0700

    Use Class

---
+++ CalculatorDriver.java
@@ -0,0 +1,7 @@
+public class CalculatorDriver{
+	public static void main(String[] args){
+	Calculator c1 = new Calculator();
+	System.out.println(c1.add(9,7));
+	System.out.println(c1.multiply(9,9));
+	}
+}
